c779a6d147f9969a9f804e709ecdf5613cdbda58
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2024-07-05 01:28:03 +0800

    refactor

--- game.js
+++ game.js
@@ -6,11 +6,6 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColour);
-    return gamePattern;
-}
-
-function playSound(color){
-    var audio = new Audio("sounds/"+color+".mp3");
-    return audio.play();
+    var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+    audio.play();
 }
