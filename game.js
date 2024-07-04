c2371e93f0a459d69bc3cb0b501d51b646088751
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2024-07-05 01:13:03 +0800

    added animation

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     return gamePattern;
 }
