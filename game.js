98da0ff5e1c90740f4f2b44f4c0e4cfbbb810982
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2024-07-05 16:03:58 +0800

    restar game added

--- game.js
+++ game.js
@@ -46,6 +46,18 @@ function checkAnswer(currentLevel){
                 },1000);
         }
     }else{
-        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function (){
+            $("body").removeClass("game-over");
+        },200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
     }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
 }
